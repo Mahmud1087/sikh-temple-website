3011397436e4d2c808bec5fbb36a9c6b5ae0d527
Mahmud1087 <abdulazeezm578@gmail.com>
2024-11-07 16:17:46 +0100

    changed the logo file path

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,16 +7,22 @@ type LogoProps = {
 };
 
 const Logo: React.FC<LogoProps> = ({
-  imageSrc = './public/logo2.jpeg',
+  imageSrc = '/logo2.jpeg',
   altText = 'Logo',
   text = 'Sikh Temple',
 }) => {
   return (
-    <div className="flex items-center space-x-2">
+    <div className='flex items-center space-x-2'>
       {imageSrc ? (
-        <img src={imageSrc} alt={altText} className="w-12 h-12 md:w-16 md:h-16" />
+        <img
+          src={imageSrc}
+          alt={altText}
+          className='w-12 h-12 md:w-16 md:h-16'
+        />
       ) : (
-        <div className="text-2xl font-bold text-blue-600 md:text-3xl">{text}</div>
+        <div className='text-2xl font-bold text-blue-600 md:text-3xl'>
+          {text}
+        </div>
       )}
     </div>
   );
